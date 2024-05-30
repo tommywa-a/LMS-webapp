5f0ca1a43169fc115c974d6d7cfb112b6c3409d7
tommywa-a <tomiwa95@gmail.com>
2024-05-30 22:15:54 +0100

    commit

---
+++ app/(group)/routeA/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RouteA = () => {
+  return (
+    <div>RouteA</div>
+  )
+}
+
+export default RouteA
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <p className="text-3xl font-medium text-sky-700">Hello World</p>
+    <div>
+      <p className="text-3xl font-medium text-sky-700">Hello World</p>
+    </div>
   )
 }
