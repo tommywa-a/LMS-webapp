9372283757a80c2855f8b079bfaec1ff53ea4f6a
tommywa-a <tomiwa95@gmail.com>
2024-06-03 16:55:12 +0100

    Added authentication

--- app/(auth)/(routes)/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const signInPage = () => {
-  return (
-    <div>This is an unprotected page</div>
-  )
-}
-
-export default signInPage
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,7 +1,13 @@
+import { UserButton } from "@clerk/nextjs"
+
 export default function Home() {
   return (
     <div>
-      <p>This is a protected page</p>
+      <div>
+        <UserButton
+          afterSignOutUrl="/"
+        />
+      </div>
     </div>
   )
 }
