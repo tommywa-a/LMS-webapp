9f42cea53aea81a539131dec640a87723df4f488
tommywa-a <tomiwa95@gmail.com>
2024-06-04 10:52:44 +0100

    Dynamic Layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -16,7 +16,7 @@ const DashboardLayout = ({
       <div className='hidden md:flex w-56 flex-col fixed inset-y-0 z-50'>
         <Sidebar />
       </div>
-      <main className='md:pl-56 h-full'>
+      <main className='md:pl-56 pt-[80px] h-full'>
         {children}
       </main>
     </div>
--- components/NavbarRoutes.tsx
+++ components/NavbarRoutes.tsx
@@ -1,14 +1,44 @@
-"use client"
+'use client'
 
 import { UserButton } from '@clerk/nextjs'
+import { usePathname } from 'next/navigation'
 import React from 'react'
+import { LogOut } from 'lucide-react'
+import Link from 'next/link'
+
+import { Button } from './ui/button'
 
 const NavbarRoutes = () => {
-  return (
-    <div className='flex gap-x-2 ml-auto'>
-      <UserButton />
-    </div>
-  )
+	const pathname = usePathname()
+
+	const isTeacherPage = pathname?.startsWith('/teacher')
+	const isPlayerPage = pathname?.startsWith('/chapter')
+
+	return (
+		<div className='flex gap-x-2 ml-auto'>
+			{isTeacherPage || isPlayerPage ? (
+				<Link href='/'>
+					<Button
+						size={'sm'}
+						variant={'ghost'}
+					>
+						<LogOut className='h-4 w-4 mr-2' />
+						Exit
+					</Button>
+				</Link>
+			) : (
+				<Link href='/teacher/courses'>
+					<Button
+						size={'sm'}
+						variant={'ghost'}
+					>
+						Teacher mode
+					</Button>
+				</Link>
+			)}
+			<UserButton afterSignOutUrl='/' />
+		</div>
+	)
 }
 
 export default NavbarRoutes
