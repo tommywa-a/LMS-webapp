979dd05b74fbe96696471051a276f6d0e3f9a788
tommywa-a <tomiwa95@gmail.com>
2024-06-05 17:36:13 +0100

    Course Edit Page UI

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,14 +1,76 @@
 import React from 'react'
 
-const CourseIdPage = ({
+import {db} from '@/lib/db'
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+
+import { IconBadge } from '@/components/IconBadge'
+import { LayoutDashboard } from 'lucide-react'
+
+const CourseIdPage = async ({
   params
 }: {
   params: {
     courseId: string
   }
 }) => {
+  const {userId} = auth()
+
+  if (!userId) {
+    return redirect('/')
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId
+    }
+  })
+
+  if (!course) {
+    return redirect('/')
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId
+  ]
+
+  const totalFields = requiredFields.length
+  const completedFields = requiredFields.filter(Boolean).length
+
+  const completionText = `${completedFields} out of ${totalFields} fields completed`
+  
   return (
-    <div>Course Id: {params.courseId}</div>
+    <div className='p-6'>
+      <div className='flex items-center justify-between'>
+        <div className='flex flex-col gap-y-2'>
+          <h1 className='text-2xl font-medium'>
+            Course setup
+          </h1>
+          <div className='flex flex-col gap-y-0'>
+            <span className='text-sm text-slate-700'>
+              Complete all fields to publish your course...
+            </span>
+            <span className='text-sm text-slate-700'>
+              {completionText}
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-16'>
+        <div>
+          <div className='flex items-center gap-x-2'>
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className='text-xl'>
+              Customize your course
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
