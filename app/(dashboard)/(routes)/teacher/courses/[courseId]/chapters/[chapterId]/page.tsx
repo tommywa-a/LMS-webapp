edc7eefc69a65d818273d87b590b50e6f4cb3810
tommywa-a <tomiwa95@gmail.com>
2024-06-12 10:46:37 +0100

    Chapter Access Form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -2,10 +2,11 @@ import { auth } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
 import { db } from "@/lib/db"
 import Link from "next/link"
-import { ArrowLeft, LayoutDashboard } from "lucide-react"
+import { ArrowLeft, Eye, LayoutDashboard, Video } from "lucide-react"
 import { IconBadge } from "@/components/IconBadge"
 import ChapterTitleForm from "./_components/ChapterTitleForm"
 import ChapterDescriptionForm from "./_components/ChapterDescriptionForm"
+import ChapterAccessForm from "./_components/ChapterAccessForm"
 
 const ChapterIdPage = async ({
 	params,
@@ -85,7 +86,28 @@ const ChapterIdPage = async ({
 							chapterId={params.chapterId}
 						/>
           </div>
+					<div>
+						<div className="flex items-center gap-x-2">
+							<IconBadge icon={Eye} />
+							<h2 className="text-xl">
+								Access Settings
+							</h2>
+						</div>
+						<ChapterAccessForm
+							initialData={chapter}
+							courseId={params.courseId}
+							chapterId={params.chapterId}
+						/>
+					</div>
         </div>
+				<div>
+					<div className="flex items-center gap-x-2">
+						<IconBadge icon={Video} />
+						<h2 className="text-xl">
+							Add a video for this chapter
+						</h2>
+					</div>
+				</div>
       </div>
 		</div>
 	)
