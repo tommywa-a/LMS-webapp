0eea0d9cb84532d1df0f5ae1d43c2fb373236d5c
tommywa-a <tomiwa95@gmail.com>
2024-07-04 17:03:59 +0100

    get-Chapter

--- actions/get-chapter.ts
+++ actions/get-chapter.ts
@@ -32,6 +32,69 @@ export const getChapter = async ({
       }
     })
 
+    const chapter = await db.chapter.findUnique({
+      where: {
+        id: chapterId,
+        isPublished: true,
+      }
+    })
+
+    if (!chapter || !course) {
+      throw new Error("Chapter or course not found")
+    }
+
+    let muxData = null
+    let attachments: Attachment[] = []
+    let nextChapter: Chapter | null = null
+
+    if (purchase) {
+      attachments = await db.attachment.findMany({
+        where: {
+          courseId: courseId
+        }
+      })
+    }
+
+    if (chapter.isFree || purchase) {
+      muxData = await db.muxData.findUnique({
+        where: {
+          chapterId
+        }
+      })
+
+      nextChapter = await db.chapter.findFirst({
+        where: {
+          courseId,
+          isPublished: true,
+          position: {
+            gt: chapter?.position,
+          }
+        },
+        orderBy: {
+          position: "asc",
+        }
+      })
+    }
+
+    const userProgress = await db.userProgress.findUnique({
+      where: {
+        userId_chapterId: {
+          userId,
+          chapterId,
+        }
+      }
+    })
+
+    return {
+      chapter,
+      course,
+      muxData,
+      attachments,
+      nextChapter,
+      userProgress,
+      purchase,
+    }
+
   } catch (error) {
     console.log("[GET_CHAPTER]", error);
     return {
--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,6 +1,9 @@
+import { getChapter } from "@/actions/get-chapter"
+import { Banner } from "@/components/banner"
+import { auth } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
 
-const ChapterIdPage = ({
+const ChapterIdPage = async ({
   params
 }: {
   params: { courseId: string, chapterId: string}
@@ -10,9 +13,43 @@ const ChapterIdPage = ({
   if (!userId) {
     return redirect("/")
   }
+
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress
+    purchase,
+  } = await getChapter({
+    userId,
+    chapterId: params.chapterId,
+    courseId: params.courseId,
+  })
+
+  if(!chapter || !course) {
+    return redirect("/")
+  }
+
+  const isLocked = !chapter.isFree && !purchase
+  const completeOnEnd= !!purchase && !userProgress?.isCompleted
   
   return (
-    <div>ChapterIdPage</div>
+    <div>
+      {userProgress?.isCompleted && (
+        <Banner
+          variant="success"
+          label="You already completed this chapter."
+        />
+      )}
+      {isLocked && (
+        <Banner
+          variant="warning"
+          label="You need to purchase this course to watch this chapter."
+        />
+      )}
+    </div>
   )
 }
 
