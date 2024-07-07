55381d9735b7f2cd1aaa332e3ed8fa556adb0b94
tommywa-a <tomiwa95@gmail.com>
2024-07-07 14:55:37 +0100

    Chapter HLS Video Player

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/VideoPlayer.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/VideoPlayer.tsx
@@ -29,9 +29,34 @@ const VideoPlayer = ({
   completeOnEnd,
   title
 }: VideoPlayerProps) => {
+  const [isReady, setIsReady] = useState(false)
+
   return (
-    <div>
-      Video Player
+    <div className="relative aspect-video">
+      {!isReady && !isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
+          <Loader2 className="h-8 w-8 animate-spin text-secondary" />
+        </div>
+      )}
+      {isLocked && (
+        <div className='absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary'>
+          <Lock className='h-8 w-8' />
+          <p className="text-sm">
+            This chapter is locked
+          </p>
+        </div>
+      )}
+      {!isLocked && (
+        <MuxPlayer
+        title={title}
+        className={cn(
+          !isReady && "hidden"
+      )}
+      onCanPlay={() => setIsReady(true)}
+      onEnded={() => {}}
+      autoPlay
+      playbackId={playbackId}
+      />
     </div>
   )
 }
