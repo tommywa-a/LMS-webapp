156707ae7076b0516141b8a3491cc08679702eb1
tommywa-a <tomiwa95@gmail.com>
2024-07-21 23:13:58 +0100

    Modified dashboard page

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -1,7 +1,33 @@
-export default function Dashboard() {
+import { getDashboardCourses } from "@/actions/get-dashboard-courses"
+import CoursesList from "@/components/CoursesList"
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
+export default async function Dashboard() {
+  const {userId} = auth()
+
+  if (!userId) {
+    return redirect("/")
+  }
+
+  const {
+    completedCourses,
+    coursesInProgress
+  } = await getDashboardCourses(userId)
+
   return (
-    <div>
-      
+    <div className="p-6 space-y-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <div>
+          {/* TODO: Info Card */}
+        </div>
+        <div>
+          {/* TODO: Info Card */}
+        </div>
+      </div>
+      <CoursesList
+        items={[...coursesInProgress, ...completedCourses]}
+      />
     </div>
   )
 }
