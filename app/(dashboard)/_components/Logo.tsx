e11d01854ad05c011c55262b63fdd3c37528054b
Tomiwa Aderibigbe <tomiwa95@gmail.com>
2024-08-28 22:57:55 +0100

    Added ability to go to homepage with logo, and added teacher ID for authorization

--- app/(dashboard)/_components/Logo.tsx
+++ app/(dashboard)/_components/Logo.tsx
@@ -1,13 +1,23 @@
+"use client"
+
 import React from 'react'
 import Image from 'next/image'
+import {useRouter} from 'next/navigation'
 
 const Logo = () => {
+  const router = useRouter()
+  const onClick = () => {
+    return router.push("/")
+  }
+
   return (
     <Image
       height={130}
       width={130}
       alt='Logo'
       src='/logo.svg'
+      onClick={onClick}
+      className='cursor-pointer'
     />
   )
 }
