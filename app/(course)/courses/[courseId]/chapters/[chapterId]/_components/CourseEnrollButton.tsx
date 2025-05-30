68163d36cd511c6ee772eae690b36b195f274fec
tommywa-a <tomiwa95@gmail.com>
2025-05-30 02:24:04 +0100

    Disabled ability for guests to enroll in course without signing in

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/CourseEnrollButton.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/CourseEnrollButton.tsx
@@ -5,6 +5,7 @@ import { formatPrice } from "@/lib/format"
 import { useState } from "react"
 import toast from "react-hot-toast"
 import axios from "axios"
+import { useUser } from "@clerk/nextjs";
 
 interface CourseEnrollButtonProps {
   price: number
@@ -16,16 +17,22 @@ const CourseEnrollButton = ({
   courseId,
 }: CourseEnrollButtonProps) => {
   const [isLoading, setIsLoading] = useState(false)
+  const { user } = useUser()
   
   const onClick = async () => {
     try {
       setIsLoading(true)
 
+      if (user?.primaryEmailAddress?.emailAddress.endsWith("@temporary.com")) {
+        toast.error("You have to sign up with a real email address to enroll in courses.")
+        return
+      }
+
       const response = await axios.post(`/api/courses/${courseId}/checkout`)
 
       window.location.assign(response.data.url)
     } catch {
-      toast.error("Something went wrong")
+      toast.error("Something went wrong while enrolling in the course.")
     } finally {
       setIsLoading(false)
     }
